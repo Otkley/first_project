1818fedf7b709ccd21aac117fbf559cfef292f63
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-12 17:47:27 -0600

    Create the Posts model, add to seeds and 2 little model validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.create!(
+  [
+    {
+      title: "Primer post!",
+      content: "Este es el contenido del primer post!"
+    },
+    {
+      title: "Segundo post o.o",
+      content: "El contenido del segundo post"
+    }
+  ]
+)
