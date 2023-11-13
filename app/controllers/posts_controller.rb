815402b7a522171cc99a5976e2b850fd554baddf
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-12 21:06:12 -0600

    set_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,17 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     @post.update(title: params[:post][:title], content: params[:post][:content])
 
     redirect_to post_path(@post)
@@ -32,10 +30,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
-
     @post.destroy
 
     redirect_to root_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
