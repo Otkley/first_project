80ed5687ef344b38a8f7b7a087905c674408969e
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-12 21:12:28 -0600

    post-update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,11 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def new
